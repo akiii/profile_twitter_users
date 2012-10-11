0f684568d75a8e7a2fe2bd3499c984b79c737411
akiii <akiii@sfc.wide.ad.jp>
2012-10-12 03:37:01 +0900

    Modified routes.

--- config/routes.rb
+++ config/routes.rb
@@ -57,4 +57,5 @@ ProfileTwitterUsers::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  match 'cron/gather/tweets' => 'tweets#gather'
 end
