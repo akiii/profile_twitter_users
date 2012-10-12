4f107b7efe1020f32a9787cb88174c7878121ff6
akiii <akiii@sfc.wide.ad.jp>
2012-10-13 02:07:39 +0900

    Added a function to gather and save tweets.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,17 +2,17 @@ class TweetsController < ApplicationController
 
   require 'twitter'
 
+  NUM_OF_COUNT = 200
+
   def gather
     max_attempts = 3
     num_attempts = 0
     begin
       num_attempts += 1
-      tweets = Twitter.list_timeline('sfc_list', 'sfc-all', :since_id => $since_tweet_id, :count => 200)
+      tweets = Twitter.list_timeline('sfc_list', 'sfc-all', { :count => NUM_OF_COUNT, :since_id => $since_tweet_id })
       tweets.each do |tweet|
-        puts tweet.created_at
-        puts tweet.user.screen_name
-        puts tweet.user.profile_image_url
-        puts tweet.text
+        params = {:text => tweet.text, :user => tweet.user.screen_name, :profile_image_url => tweet.user.profile_image_url, :created_at => tweet.created_at}
+        Tweet.create(params) unless Tweet.exists?(params)
       end
       $since_tweet_id = tweets[0].id
     rescue Twitter::Error::TooManyRequests => error
